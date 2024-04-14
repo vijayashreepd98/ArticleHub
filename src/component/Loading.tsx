f781eaa6f195f5c7b058faba5e036ee515a52062
vijayashreepd98@gmail.com <vijayashreepd98@gmail.com>
2024-04-15 03:35:42 +0530

    Loading component

---
+++ src/component/Loading.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function Loading() {
+  return <LoadingContainer>Loading...</LoadingContainer>;
+}
+
+const LoadingContainer = styled.h1`
+  text-align: center;
+  margin: auto;
+  font-size: 2rem;
+`;
