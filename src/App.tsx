a5b79702bedd2506c93c55d42bae5b8c9cdbfd92
vijayashreepd98@gmail.com <vijayashreepd98@gmail.com>
2024-04-16 16:48:23 +0530

    fixes on code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import RouteData from "./global/routes";
 import { ArticleContextProvider } from "./context/ContextProvider";
-import ArticleList from "./pages/ArticleList";
 
 function App() {
- 
   return (
     <Router>
       <ArticleContextProvider>
         <Routes>
           {RouteData.map(({ path, page }) => {
-            return <Route path={path} Component={page} key={path} />;
+            return (
+              <Route
+                path={path}
+                element={React.createElement(page)}
+                key={path}
+              />
+            );
           })}
-                      <Route path={"path"} Component={ArticleList} key={"path"} />;
-
         </Routes>
       </ArticleContextProvider>
     </Router>
